cf0718d53b2c88743262e6ff74b5f3bd2bfc52ef
Sarjan Shah <shahsarjan968@gmail.com>
2026-01-06 12:18:21 +0530

    Fix STATIC_ROOT for collectstatic on Render

--- pleaseBack/backend/settings.py
+++ pleaseBack/backend/settings.py
@@ -44,6 +44,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f80d@0jy2n7n2@(&cbf3&^sugi@2ad!(7jvufc+!azvfb9+bh('
+DEBUG = os.getenv("DEBUG") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
