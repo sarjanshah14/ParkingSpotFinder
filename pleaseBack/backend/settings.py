d07cb3c2bcf885ca0690269fecc273fd6f4c2860
Sarjan Shah <shahsarjan968@gmail.com>
2026-01-06 14:25:07 +0530

    fixed render

--- pleaseBack/backend/settings.py
+++ pleaseBack/backend/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -202,3 +203,5 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
